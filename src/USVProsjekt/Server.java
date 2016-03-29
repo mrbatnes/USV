4bb3697fd546b485969738f15605675008d9fafc
vegkams <vegard.kamsvag@stud.hials.no>
2016-03-29 14:05:14 +0200

    Laget Server klasse (work in progress)

---
+++ src/USVProsjekt/Server.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package USVProsjekt;
+
+import java.io.BufferedReader;
+import java.io.PrintStream;
+import java.net.Socket;
+
+/**
+ *
+ * @author vegard
+ */
+public class Server extends Thread{
+    
+    private Socket csocket;
+    private BufferedReader inFromServer;
+    private PrintStream printStream;
+    
+    
+}
