646d0673c8b434b9745b52fa954d747cb534b367
lars-harald <larsharald_b@hotmail.com>
2016-11-24 09:14:43 +0100

    init

---
+++ lib/copyDependencies.cmd
@@ -0,0 +1,15 @@
+copy D:\DATADIR\Var\maven\repository\colt\colt\1.2.0\colt-1.2.0.jar .\
+copy D:\DATADIR\Var\maven\repository\concurrent\concurrent\1.3.4\concurrent-1.3.4.jar .\
+copy D:\DATADIR\Var\maven\repository\com\seventytwomiles\architecture-rules\2.1.1\architecture-rules-2.1.1.jar .\
+copy D:\DATADIR\Var\maven\repository\commons-digester\commons-digester\1.8\commons-digester-1.8.jar .\
+copy D:\DATADIR\Var\maven\repository\commons-beanutils\commons-beanutils\1.7.0\commons-beanutils-1.7.0.jar .\
+copy D:\DATADIR\Var\maven\repository\jdepend\jdepend\2.9.1\jdepend-2.9.1.jar .\
+copy D:\DATADIR\Var\maven\repository\jboss\javassist\3.0\javassist-3.0.jar .\
+copy D:\DATADIR\Var\maven\repository\commons-logging\commons-logging\1.1.1\commons-logging-1.1.1.jar .\
+copy D:\DATADIR\Var\maven\repository\junit\junit\4.11\junit-4.11.jar .\
+copy D:\DATADIR\Var\maven\repository\org\hamcrest\hamcrest-core\1.3\hamcrest-core-1.3.jar .\
+copy D:\DATADIR\Var\maven\repository\log4j\log4j\1.2.14\log4j-1.2.14.jar .\
+copy D:\DATADIR\Var\maven\repository\net\sourceforge\csparsej\csparsej\1.1.1\csparsej-1.1.1.jar .\
+copy D:\DATADIR\Var\maven\repository\org\apache\commons\commons-lang3\3.2\commons-lang3-3.2.jar .\
+copy D:\DATADIR\Var\maven\repository\org\apache\commons\commons-math3\3.3\commons-math3-3.3.jar .\
+copy D:\DATADIR\Var\maven\repository\org\ostermiller\utils\1.07.00\utils-1.07.00.jar .\
