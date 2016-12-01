dc55d1f722d12543d4f790bc230be49bc81126d5
lars-harald <larsharald_b@hotmail.com>
2016-12-01 12:56:33 +0100

    testing

--- src/usv/ThrustAllocUSV.java
+++ src/usv/ThrustAllocUSV.java
@@ -20,6 +20,8 @@ import com.joptimizer.optimizers.LPOptimizationRequest;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import org.apache.commons.math3.linear.MatrixUtils;
+import org.apache.commons.math3.linear.RealMatrix;
 
 /**
  *
@@ -88,99 +90,116 @@ public class ThrustAllocUSV {
     private static double PI = Math.PI;
     private double thrusterMaxForce = 50; // Newton
     private double epsilon = thrusterMaxForce / 100d; // 1% error
+    private BlockRealMatrix P; // W and Q matrix together
     private double[][] B;
     private double[][] W;
     private double[][] Q;
     private double[] s;
     private double[] u;
     private double[] u_max;
+    private double[] beta;
+
+    private double Lx1;
+    private double Ly1;
+    private double Lx2;
+    private double Ly2;
+    private double Lx3;
+    private double Ly3;
+
     private BlockRealMatrix A;
-    //private BlockRealMatrix B;
-    private ArrayRealVector tau;
     private ArrayRealVector b;
     private PDQuadraticMultivariateRealFunction objectiveFunction;
     private ConvexMultivariateRealFunction[] inequalities;
     private JOptimizer opt;
 
     // illigal thruster angles contercloskwise start-end pairs
-    private double[] constrainVectorM1 = {/*rad(-85d), rad(-95d), */rad(85d), rad(95d)};
+    private double[] constrainVectorM1 = new double[0];//{/*rad(-85d), rad(-95d), */rad(85d), rad(95d)};
     private double[] constrainVectorM2 = new double[0];//{rad(85d), rad(95d)};
     private double[] constrainVectorM3 = new double[0];//{rad(-95), rad(-85)};
 
     public ThrustAllocUSV() {
         //    x1   y1    x2     y2    x3    y3  slack   q weight
-        setup(0d, 1.5d, -.5d, -.5d, .5d, -.5d, 0.001d, 100d);
+        setup(0d, 1.5d, -.5d, -.5d, .5d, -.5d, new double[]{10d, 10d, 10d});
     }
 
-    private void setup(double Lx1, double Ly1, double Lx2, double Ly2, double Lx3, double Ly3, double slack, double q) {
+    private void setup(double Lx1, double Ly1, double Lx2, double Ly2, double Lx3, double Ly3, double[] q) {
 
-        // position matrix
-        B = new double[][]{
-            {1d, 0d, 1d, 0d, 1d, 0d},
-            {0d, -1d, 0d, -1d, 0d, -1d},
-            {-Lx1, -Ly1, -Lx2, -Ly2, -Lx3, -Ly3}};
+        this.Lx1 = Lx1;
+        this.Ly1 = Ly1;
+        this.Lx2 = Lx2;
+        this.Ly2 = Ly2;
+        this.Lx3 = Lx3;
+        this.Ly3 = Ly3;
 
-        // power weight matrix
-        W = new double[][]{
-            {1d, 0d, 0d, 0d, 0d, 0d},
-            {0d, 1d, 0d, 0d, 0d, 0d},
-            {0d, 0d, 1d, 0d, 0d, 0d},
-            {0d, 0d, 0d, 1d, 0d, 0d},
-            {0d, 0d, 0d, 0d, 1d, 0d},
-            {0d, 0d, 0d, 0d, 0d, 1d}
-        };
+        // initial position matrix
+        B = getConfigurationMatrix_B(this.Lx1, this.Ly1, this.Lx2, this.Ly2, this.Lx3, this.Ly3, false);
+        
+        System.out.println("********** B MATRIX");
+        for(double[] b : B){
+            System.out.println(Arrays.toString(b));
+        }
+        // power weight matrix (identity matrix because identical motors)
+        W = MatrixUtils.createRealIdentityMatrix(6).getData();
 
         // slack variable cost matrix (Q >> W > 0)
-        Q = new double[][]{
-            {q, 0d, 0d, 0d, 0d, 0d},
-            {0d, q, 0d, 0d, 0d, 0d},
-            {0d, 0d, q, 0d, 0d, 0d},
-            {0d, 0d, 0d, q, 0d, 0d},
-            {0d, 0d, 0d, 0d, q, 0d},
-            {0d, 0d, 0d, 0d, 0d, q}
-        };
+        Q = MatrixUtils.createRealDiagonalMatrix(q).getData();
 
-        // slack vector
-        s = new double[Q.length];
-        for (int i = 0; i < s.length; i++) {
-            s[i] = slack;
+        P = new BlockRealMatrix(new double[9][9]);
+        P.setSubMatrix(W, 0, 0);
+        P.setSubMatrix(Q, 6, 6);
+        
+        System.out.println("************* P MATRIX");
+        for(double[] p : P.getData()){
+            System.out.println(Arrays.toString(p));
         }
 
-        // force vector
-        tau = new ArrayRealVector(new double[3]);
-
-        // u variable vector
-        u = new double[W.length];
+        
 
         // create polygon of thruster forces (linear approx)
-        Double[][] m1 = getConstrainMatrixForAzimuthThruster(thrusterMaxForce, epsilon, constrainVectorM1);
-        Double[][] m2 = getConstrainMatrixForAzimuthThruster(thrusterMaxForce, epsilon, constrainVectorM2);
-        Double[][] m3 = getConstrainMatrixForAzimuthThruster(thrusterMaxForce, epsilon, constrainVectorM3);
+        double[][] m1 = getConstrainMatrixForAzimuthThruster(thrusterMaxForce, epsilon, constrainVectorM1);
+        double[][] m2 = getConstrainMatrixForAzimuthThruster(thrusterMaxForce, epsilon, constrainVectorM2);
+        double[][] m3 = getConstrainMatrixForAzimuthThruster(thrusterMaxForce, epsilon, constrainVectorM3);
 
-        List<Double[][]> constrainList = new ArrayList<>();
+        List<double[][]> constrainList = new ArrayList<>();
         constrainList.add(m1);
         constrainList.add(m2);
         constrainList.add(m3);
 
         // get A matrix
+        // unequalities A*u <= b
         A = getMatrix_A(constrainList);
-        b = getMatrix_B(constrainList);
+        System.out.println("********** A MATRIX");
+        for(double[] a : A.getData()){
+            System.out.println(Arrays.toString(a));
+        }
+        
+        System.out.println("********** b VECTOR");
+        b = getConstrainVector_b(constrainList);
+        for(double B : b.toArray()){
+            System.out.println(B);
+        }
+        beta = new double[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1};
 
         // create objective function
-        //                                                        S    beta*u_max
-        objectiveFunction = new PDQuadraticMultivariateRealFunction(W, null, 0);
+        //                                                             beta*u_max
+        objectiveFunction = new PDQuadraticMultivariateRealFunction(P.getData(), null, 0);
 
         // set equality B*u = tau
         inequalities = new ConvexMultivariateRealFunction[A.getRowDimension()];
         opt = new JOptimizer();
     }
 
-    public double[] calculateOutput(double[] tau) throws Exception {
+    public double[] calculateOutput(double[] tau, boolean travelMode) throws Exception {
+
+        //  set the configuration matrix from travelmode or not
+        B = getConfigurationMatrix_B(Lx1, Ly1, Lx2, Ly2, Lx3, Ly3, travelMode);
+        System.out.println("****** B POS MATRIX");
+        for(double[] b : B){
+            System.out.println(Arrays.toString(b));
+        }
 
         for (int i = 0; i < A.getRowDimension(); i++) {
             // hver ulikhet settes opp
-            //System.out.println(Arrays.toString(A.getRow(i))+ "   b = " + b.getEntry(i));
-
             inequalities[i] = new LinearMultivariateRealFunction(A.getRow(i), -b.getEntry(i));
         }
 
@@ -203,17 +222,24 @@ public class ThrustAllocUSV {
 
         // optimalisering
         opt.setOptimizationRequest(or);
-        int returnCode = opt.optimize();
+        int returnCode = 0;
+                
+        try {
+            returnCode = opt.optimize();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         if (returnCode == OptimizationResponse.FAILED) {
             System.out.println("FAILED");
+            return null;
+        } else {
+            return opt.getOptimizationResponse().getSolution();
         }
-
-        return opt.getOptimizationResponse().getSolution();
 
     }
 
-    private Double[][] getConstrainMatrixForAzimuthThruster(double R, double epsilon, double[] constrain) {
+    private double[][] getConstrainMatrixForAzimuthThruster(double R, double epsilon, double[] constrain) {
         // constains is valid for thruster
         int constrainPairs = 0;
         int constIndex = 0;
@@ -222,78 +248,134 @@ public class ThrustAllocUSV {
         }
 
         // N number of polygons required for acciving the epsilon error percent
-        Double N = round(PI / acos(1 - (epsilon / R)));
+        double N = round(PI / acos(1 - (epsilon / R)));
         double r = R * cos(PI / N); // inner radius of thruster region linearized
 
-        Double[][] m = new Double[N.intValue() + constrain.length][3];
+        double[][] m = new double[(int) N + constrain.length][3];
         int k;
         for (k = 0;
                 k < N;
                 k++) {
             double phiK = (2 * k + 1) * PI / N;
 
-            m[k] = new Double[]{cos(phiK), sin(phiK), r};
+            m[k] = new double[]{cos(phiK), sin(phiK), r};
 
         }
-
+/*
         // add constrain angles if present
         if (constrainPairs > 0) {
             for (int i = 0; k < m.length && i < constrainPairs; i++) {
-                m[k++] = new Double[]{sin(constrain[2 * i]), -cos(constrain[2 * i]), 0d};
-                m[k++] = new Double[]{-sin(constrain[2 * i + 1]), cos(constrain[2 * i + 1]), 0d};
+                m[k++] = new double[]{sin(constrain[2 * i]), -cos(constrain[2 * i]), 0d};
+                m[k++] = new double[]{-sin(constrain[2 * i + 1]), cos(constrain[2 * i + 1]), 0d};
 
             }
-        }
+        }*/
         return m;
     }
 
-    private BlockRealMatrix getMatrix_A(List<Double[][]> constrains) {
-        int rows = 0;
-        int columns = 2;
+    /**
+     * get the unequalities matrix A "A*u <= b"
+     *
+     * @param constrains the constrains matrix for each thruster in a list
+     * @return constrainMatrix A
+     */
+    private BlockRealMatrix getMatrix_A(List<double[][]> constrains) {
 
-        for (Double[][] d : constrains) {
+        int columns = 6;
+        int columnsConstraint = 2 * constrains.size();
+        int rows = 12; // initial 12 rows
+
+        // add rows for unequalities / constains
+        for (double[][] d : constrains) {
             rows = rows + d.length;
         }
 
-        BlockRealMatrix A = new BlockRealMatrix(new double[rows][columns * constrains.size()]);
+        // dynamic column size
+        columns = columns + columnsConstraint;
+        BlockRealMatrix A = new BlockRealMatrix(new double[rows][columns]);
+
+        // iterate trough all constrain matrixes
         int index = 0;
-        for (int i = 1;
+        for (int i = 0;
                 i < constrains.size();
                 i++) {
-            Double[][] m = (constrains.get(i));
-            double[][] mm = new double[m.length][columns];
-
-            for (int j = 0; j < mm.length; j++) {
-                mm[j] = new double[]{m[j][0], m[j][1]};
-            }
-
-            for (int k = 0;
-                    k < mm.length;
-                    k++) {
-                ArrayRealVector arv = new ArrayRealVector(constrains.size() * 2);
-                arv.setSubVector((i - 1) * 2, mm[k]);
-                A.setRowVector(index, arv);
-
-                //System.out.println("row nr: " + index + " " +Arrays.toString(A.getRow(index)));
-                index++;
-
-            }
+            // get constrain for each thruster
+            BlockRealMatrix C = new BlockRealMatrix(constrains.get(i));
+            C = C.getSubMatrix(0, (C.getRowDimension() - 1), 0, 1);
+            index = index + C.getRowDimension();
+            // set all constraints in a matrix
+            A.setSubMatrix(C.getData(), i * C.getRowDimension(), 2 * i);
         }
+
+        /*
+        total matrix A in this format:
+        
+        | A  0   0|   |u|    |b|
+        |-I  0  -1| * |s| <= |0|
+        | I  0  -1|   |ü|    |0|
+        
+         */
+        RealMatrix I_6x6 = MatrixUtils.createRealIdentityMatrix(6); // identity 6x6
+        RealMatrix minus_I_6x6 = MatrixUtils.createRealIdentityMatrix(6).scalarMultiply(-1d); // identity 6x6 * -1
+
+        A.setSubMatrix(minus_I_6x6.getData(), index, 0);
+        A.setSubMatrix(matrix(12, 3, -1d), index, columnsConstraint + 3);
+        index = index + minus_I_6x6.getRowDimension();
+        A.setSubMatrix(I_6x6.getData(), index, 0);
+
+        /* print result
+        for (int i = 0; i < A.getRowDimension(); i++) {
+            System.out.println("row nr: " + i + " " + Arrays.toString(A.getRow(i)));
+        }
+         */
         return A;
     }
 
-    private ArrayRealVector getMatrix_B(List<Double[][]> constrains) {
+    /**
+     * get configuration matrix B. configuration of thrusters from senter of
+     * gravity and the effection of vektor tau
+     *
+     * @param Lx1
+     * @param Ly1
+     * @param Lx2
+     * @param Ly2
+     * @param Lx3
+     * @param Ly3
+     * @param isInTravelMode exclude the front thruster if in travelmode
+     * @return
+     */
+    private double[][] getConfigurationMatrix_B(double Lx1, double Ly1, double Lx2, double Ly2, double Lx3, double Ly3, boolean isInTravelMode) {
+        if (isInTravelMode) {
+            return new double[][]{
+                {0d, 0d, 1d, 0d, 1d, 0d, -1d, 0d, 0d, 0d, 0d, 0d},
+                {0d, 0d, 0d, -1d, 0d, -1d, 0d, -1d, 0d, 0d, 0d, 0d},
+                {0d, 0d, -Lx2, -Ly2, -Lx3, -Ly3, 0d, 0d, -1d, 0d, 0d, 0d}};
+        } else {
+            return new double[][]{
+                {1d, 0d, 1d, 0d, 1d, 0d, -1d, 0d, 0d, 0d, 0d, 0d},
+                {0d, -1d, 0d, -1d, 0d, -1d, 0d, -1d, 0d, 0d, 0d, 0d},
+                {-Lx1, -Ly1, -Lx2, -Ly2, -Lx3, -Ly3, 0d, 0d, -1d, 0d, 0d, 0d}};
+        }
+    }
 
-        int rows = 0;
+    /**
+     * get constrain vector b from unequality A*u <= b
+     *
+     * @param constrains all the unequalities from thrusters in a list
+     * @return
+     */
+    private ArrayRealVector getConstrainVector_b(List<double[][]> constrains) {
 
-        for (Double[][] d : constrains) {
+        int rows = 12; // initial 12 rows
+
+        for (double[][] d : constrains) {
             rows = rows + d.length;
         }
 
         ArrayRealVector b = new ArrayRealVector(new double[rows]);
         int i = 0;
-        for (Double[][] d : constrains) {
-            for (Double[] dd : d) {
+        for (double[][] d : constrains) {
+            for (double[] dd : d) {
                 if (!(dd.length != 3 && i < rows)) {
                     b.setEntry(i, dd[2]);
                     i++;
@@ -305,27 +387,48 @@ public class ThrustAllocUSV {
         return b;
     }
 
-    private static double[][] zeros(int rows, int columns) {
-        return new double[rows][columns];
+    /**
+     * get thust forces and angles from a vector
+     *
+     * @param r input vector length 6
+     * @return force - angle pairs of thruster in radians
+     */
+    private double[] getThrustAndAnglesRad(double[] r) {
+        double u1 = Math.sqrt(r[0] * r[0] + r[1] * r[1]);
+        double u2 = Math.sqrt(r[2] * r[2] + r[3] * r[3]);
+        double u3 = Math.sqrt(r[4] * r[4] + r[5] * r[5]);
+
+        double phi1 = Math.atan2(r[1], r[0]);
+        double phi2 = Math.atan2(r[3], r[2]);
+        double phi3 = Math.atan2(r[5], r[4]);
+
+        return new double[]{u1, phi1, u2, phi2, u3, phi3};
     }
 
-    private static double[][] ones(int rows, int columns) {
-        double[][] result = new double[rows][columns];
+    /**
+     * get thust forces and angles from a vector
+     *
+     * @param r input vector length 6
+     * @return force - angle pairs of thruster in degrees
+     */
+    private double[] getThrustAndAnglesDeg(double[] r) {
+        double u1 = Math.sqrt(r[0] * r[0] + r[1] * r[1]);
+        double u2 = Math.sqrt(r[2] * r[2] + r[3] * r[3]);
+        double u3 = Math.sqrt(r[4] * r[4] + r[5] * r[5]);
 
-        for (double[] result1 : result) {
-            for (double v : result1) {
-                v = 1d;
-            }
-        }
-        return result;
+        double phi1 = Math.atan2(r[1], r[0]);
+        double phi2 = Math.atan2(r[3], r[2]);
+        double phi3 = Math.atan2(r[5], r[4]);
+
+        return new double[]{u1, deg(phi1), u2, deg(phi2), u3, deg(phi3)};
     }
 
-    private static double[][] minusOnes(int rows, int columns) {
+    private static double[][] matrix(int rows, int columns, double value) {
         double[][] result = new double[rows][columns];
 
-        for (double[] result1 : result) {
-            for (int j = 0; j < result1.length; j++) {
-                result1[j] = -1d;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                result[i][j] = value;
             }
         }
         return result;
--- src/usv/USV.java
+++ src/usv/USV.java
@@ -20,27 +20,20 @@ public class USV {
      */
     public static void main(String[] args) {
         ThrustAllocUSV t = new ThrustAllocUSV();
-        
+        org.apache.log4j.BasicConfigurator.configure();
+
         try {
-            double[] r = t.calculateOutput(new double[]{0,0,1});
-            double u1 = Math.sqrt(r[0]*r[0]+r[1]*r[1]);
-            double u2 = Math.sqrt(r[2]*r[2]+r[3]*r[3]);
-            double u3 = Math.sqrt(r[4]*r[4]+r[5]*r[5]);
-       
-            double phi1 = Math.atan2(r[1],r[0]);
-            double phi2 = Math.atan2(r[3],r[2]);
-            double phi3 = Math.atan2(r[5],r[4]);
-            
-            System.out.println(Arrays.toString(r));
-            System.out.println(" u1: " + u1 +" u2: " + u2 +" u3: " + u3);
-            System.out.println(" phi1: " + phi1*360./(2*Math.PI) +" phi2: " + phi2*360./(2*Math.PI) +" phi3: " + phi3*360./(2*Math.PI));
+            double[] r = t.calculateOutput(new double[]{1, 0, 0}, false);
+            if (r != null) {
+                System.out.println(Arrays.toString(r));
+            } else {
+                System.out.println("error calculating output");
+            }
+
         } catch (Exception ex) {
             Logger.getLogger(USV.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-  
-    }
-    
 
-    
+    }
+
 }
